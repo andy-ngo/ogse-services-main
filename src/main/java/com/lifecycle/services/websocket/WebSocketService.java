a785dd5dfe97ed420c271df82b1559b134881358
andy-ngo <andyngo03@gmail.com>
2022-11-22 01:14:47 -0500

    feat: removed commented code

--- src/main/java/com/lifecycle/services/websocket/WebSocketService.java
+++ src/main/java/com/lifecycle/services/websocket/WebSocketService.java
@@ -36,7 +36,7 @@ public class WebSocketService {
     //call from visualization folder to call specific uuid
     //grab uuid messages.log file and send it to client
 
-    //run the web socket and then make it run frontend
+    //run the web socket and then make it run front end
     //do folder.get(messages.log) to get a file
 
     //method to start connection
@@ -60,7 +60,7 @@ public class WebSocketService {
         return resultFile;
     }
 
-    //method stream to frontend
+    //method stream to front end
     public void sendResults(Session session, File file)
     {
         /* 
@@ -85,41 +85,5 @@ public class WebSocketService {
     {
 
     }
-    */
-
-    /* 
-	@OnOpen
-	public void onOpen(Session session, @PathParam("file_name") String name) throws IOException {
-		// Get session and WebSocket connection
-        this.session = session;
-        simulations.put(session.getId(),name);
-
-        Message message = new Message();
-        message.setField(name);
-        message.setContent("Simulated");
-        broadcast(message);
-	}
-	
-	@OnMessage
-    public void onMessage(Session session, Message message) throws IOException {
-        // Handle new messages
-        message.setFrom(simulations.get(session.getId()));
-        broadcast(message);
-    }
-
-    @OnClose
-    public void onClose(Session session) throws IOException {
-        // WebSocket connection closes
-    }
-
-    @OnError
-    public void onError(Session session, Throwable throwable) {
-        // Do error handling here
-    }
-
-    private static void broadcast(Message message) throws IOException,EncodeException{
-        // Display the simulation data
-    }
-    */
-    
+    */    
 }
