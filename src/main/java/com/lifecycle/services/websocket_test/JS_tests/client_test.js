3fcf0d58190bec93390e391e668638d611c1173e
andy-ngo <andyngo03@gmail.com>
2022-11-24 00:11:29 -0500

    feat: testing

--- src/main/java/com/lifecycle/services/websocket_test/JS_tests/client_test.js
+++ src/main/java/com/lifecycle/services/websocket_test/JS_tests/client_test.js
@@ -1,8 +1,8 @@
-<html>
+/*<html>
     <head>
         <title>Chat WebSocket</title>
-        <script src="resources/js/sockjs-0.3.4.js"></script>
-        <script src="resources/js/stomp.js"></script>
+        <script src="sockjs-0.3.4.js"></script>
+        <script src="stomp.js"></script>
         <script type="text/javascript">
             var stompClient = null;
             
@@ -73,3 +73,28 @@
 
     </body>
 </html>
+*/
+function connect() {
+    var socket = new WebSocket('ws://localhost:8080/greeting');
+    ws = Stomp.over(socket);
+
+    ws.connect({}, function(frame) {
+        ws.subscribe("/user/queue/errors", function(message) {
+            alert("Error " + message.body);
+        });
+
+        ws.subscribe("/user/queue/reply", function(message) {
+            alert("Message " + message.body);
+        });
+    }, function(error) {
+        alert("STOMP error " + error);
+    });
+}
+
+function disconnect() {
+    if (ws != null) {
+        ws.close();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
