e431bbd5410014f25be7e21b75ac363be8a8988f
andy-ngo <andyngo03@gmail.com>
2022-11-28 02:15:36 -0500

    feat: added in websocket config

--- src/main/java/com/WebConfig.java
+++ src/main/java/com/WebConfig.java
@@ -2,17 +2,21 @@ package com;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @Configuration
 @EnableWebMvc
+@EnableWebSocketMessageBroker
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
@@ -20,6 +24,22 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
 		        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
+    
+    /*
+    //Websocket configuration
+    @Override
+    public void registerStompEndpoints(final StompEndpointRegistry registry)
+    {
+        registry.addEndpoint("/chat-example").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(final MessageBrokerRegistry registry)
+    {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
+    }
+    */
 
     @Bean
     public ClassLoaderTemplateResolver templateResolver() {
