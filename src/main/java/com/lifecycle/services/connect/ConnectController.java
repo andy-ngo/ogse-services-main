c48fb7b83381dbe58acc9349242246019beb6e4c
tylergwm2000 <tylergwm@gmail.com>
2022-12-04 21:30:41 -0500

    Fix run error & implement functioning REST message

--- src/main/java/com/lifecycle/services/connect/ConnectController.java
+++ src/main/java/com/lifecycle/services/connect/ConnectController.java
@@ -1,9 +1,12 @@
 package com.lifecycle.services.connect;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.lifecycle.components.rest.Controller;
 
@@ -20,4 +23,13 @@ private final ConnectService cService;
     public void connect(@RequestParam(value = "uuid", required = true) String uuid) throws Exception {    	
 		System.out.println(uuid + " REST API connected!");
     }
+	
+	@GetMapping(path="/demo/connect", produces = MediaType.TEXT_HTML_VALUE)
+    public ModelAndView demoHtml() throws Exception {
+		ModelAndView mv = new ModelAndView();
+		
+        mv.setViewName("lifecycle/demo");
+        
+        return mv;
+	}
 }
